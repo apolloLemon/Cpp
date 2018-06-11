8b1989778e954b56b1dadb0083ff99f6b3c6c18c
Matthew Coyle <matthew@coyle.fr>
2018-06-11 15:49:12 +0200

    corrected range function <=max

--- Derek_Banas_CppTut/vid03p02.cpp
+++ Derek_Banas_CppTut/vid03p02.cpp
@@ -6,16 +6,16 @@
 std::vector<int> Range (int start, int max, int step);
 
 int main (){
-	std::vector<int> a = Range(0,100,3);
+	std::vector<int> a = Range(1,10,1);
 	for(auto x: a) std::cout<<x<<std::endl; 
-	
+
 	return 0;
 }
 
 //FUNCTIONS
 std::vector<int> Range (int start, int max, int step){
 	std::vector<int> out;
-	for(int i=start;i<max;i+=step)
+	for(int i=start;i<=max;i+=step)
 		out.push_back(i);
 	return out;
 }
