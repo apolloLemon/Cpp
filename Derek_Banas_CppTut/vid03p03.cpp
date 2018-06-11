d9b8eb05d170ec57205a7031ae61f4a429ee1a4c
Matthew Coyle <matthew@coyle.fr>
2018-06-11 15:49:32 +0200

    DBanas Tut3 Pb3

---
+++ Derek_Banas_CppTut/vid03p03.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <numeric>
+
+std::vector<int> Range (int start, int max, int step);
+
+int main (){
+	double invest, rate; 
+	std::cout << "How much to invest : "; std::cin >> invest;
+	std::cout << "Intrest Rate : "; std::cin >> rate;
+	rate *= .01;
+
+	for(auto x: Range(1,10,1))
+		invest+=(invest*rate);
+
+	std::cout << "Value after 10 years : "<<invest<<std::endl;
+
+	return 0;
+}
+
+//FUNCTIONS
+std::vector<int> Range (int start, int max, int step){
+	std::vector<int> out;
+	for(int i=start;i<= max;i+=step)
+		out.push_back(i);
+	return out;
+}
+//END FUNCTIONS
