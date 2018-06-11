387579c1459b7aa906e1b1feb03b470a2f800947
Matthew Coyle <matthew@coyle.fr>
2018-06-11 15:27:03 +0200

    DBanas Tut3 Pb2

---
+++ Derek_Banas_CppTut/vid03p02.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <numeric>
+
+std::vector<int> Range (int start, int max, int step);
+
+int main (){
+	std::vector<int> a = Range(0,100,3);
+	for(auto x: a) std::cout<<x<<std::endl; 
+	
+	return 0;
+}
+
+//FUNCTIONS
+std::vector<int> Range (int start, int max, int step){
+	std::vector<int> out;
+	for(int i=start;i<max;i+=step)
+		out.push_back(i);
+	return out;
+}
+//END FUNCTIONS
