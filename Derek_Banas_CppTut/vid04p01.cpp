55a5e52812a8975ebcde5c7d93910acfc89e7080
Matthew Coyle <matthew@coyle.fr>
2018-06-19 12:08:58 +0200

    Pine Tree Problem with following hints

--- Derek_Banas_CppTut/vid04p01.cpp
+++ Derek_Banas_CppTut/vid04p01.cpp
@@ -15,25 +15,35 @@ How tall is the tree: 5
     #
 */
 
+/* Hints:
+	- Decrement spaces by one each loop
+	- Increment hashes by 2 each lopp
+	- save spaces to the stump by calculating tree height-1
+	- Decrement from tree height until equals zero (*-1)
+	- Print spaces and then hashes for each row
+	- Print stump spaces and then 1 hash
+*/
+
 int main (){
 
-	int nTreeHight;
+	int nTreeHight, maxSpaces, nSpaces, nHashes;
 	std::cout << "How tall is the tree: ";std::cin>>nTreeHight;
+	maxSpaces = nTreeHight-1;
+	nSpaces = maxSpaces;
+	nHashes = 1;
 
 	for(auto i: Range(0,nTreeHight,1)){
 
-		//Trying to use continue and Break
-		/*int iSpace = nTreeHight-1 , iHash = (nTreeHight-1)*2+1; //Max number of spaces of hashtags
-		//Spaces loop
-		while(iSpace){
-			std::cout<<" ";
-			iSpace--;
-		}
-		std::cout<<"#";*/
+		if(i==nTreeHight) continue;
+		for(auto j: Range(1,nSpaces,1)) std::cout<<" ";
+		for(auto k: Range(1,nHashes,1)) std::cout<<"#";
+
+		nSpaces--; nHashes+=2; std::cout<<std::endl;
 
 
 		//Combo space and # done before hints
-		int d = i;
+		// Doesn't need extra forloop
+		/*int d = i;
 		if(d==nTreeHight) d=0;
 
 		int w = 0;
@@ -42,8 +52,12 @@ int main (){
 			else std::cout <<" ";
 			w++;
 		}
-		std::cout<<std::endl;
+		std::cout<<std::endl;*/
 	}
+
+	//Added forloop as last step of hints
+	for(auto x: Range(1,maxSpaces,1)) std::cout<<" ";
+	std::cout<<"#\n";
 
 	return 0;
 }
