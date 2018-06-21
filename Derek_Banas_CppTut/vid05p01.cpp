86e6a28f8ff03c8083c5571a0b907f51c9f4c0cd
Matthew Coyle <matthew@coyle.fr>
2018-06-21 16:07:34 +0200

    made it to work on upper and lower case

--- Derek_Banas_CppTut/vid05p01.cpp
+++ Derek_Banas_CppTut/vid05p01.cpp
@@ -8,15 +8,20 @@ int main () {
 	std::cout<<"Enter word: ";std::cin>>usrin;
 
 	for(char a: usrin) {
-		out += std::to_string((int)a);
+		out += std::to_string((int)a-23);
 	}
 	std::cout<<out<<std::endl;
 
 	//resetting to convert back
 	usrin = out; out="";
+	
+
 	for(int i=0;i<usrin.length();i+=2){
-		out += (char)((int)usrin[i]-'0')*10+((int)usrin[i+1]-'0');
+		out += (char)((int)usrin[i]-'0')*10+((int)usrin[i+1]-'0')+23;
 	}
 	std::cout<<out<<std::endl;
+
+
+
 	return 0;
 }
