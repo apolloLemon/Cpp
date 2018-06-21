999a4cb87f1d1523dbc71538cfbdd4cb9393d172
Matthew Coyle <matthew@coyle.fr>
2018-06-21 16:03:55 +0200

    added unscramble

--- Derek_Banas_CppTut/vid05p01.cpp
+++ Derek_Banas_CppTut/vid05p01.cpp
@@ -8,9 +8,15 @@ int main () {
 	std::cout<<"Enter word: ";std::cin>>usrin;
 
 	for(char a: usrin) {
-		out = out + std::to_string((int)a);
+		out += std::to_string((int)a);
 	}
+	std::cout<<out<<std::endl;
 
+	//resetting to convert back
+	usrin = out; out="";
+	for(int i=0;i<usrin.length();i+=2){
+		out += (char)((int)usrin[i]-'0')*10+((int)usrin[i+1]-'0');
+	}
 	std::cout<<out<<std::endl;
 	return 0;
 }
