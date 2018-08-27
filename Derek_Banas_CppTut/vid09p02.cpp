a2f234e8f60f7ac516b83e21040d3926cc3bae76
Matthew Coyle <matthew@coyle.fr>
2018-08-27 13:19:00 +0200

    list of Fibs

--- Derek_Banas_CppTut/vid09p02.cpp
+++ Derek_Banas_CppTut/vid09p02.cpp
@@ -2,12 +2,32 @@
 #include <algorithm>
 #include <iostream>
 
+std::vector<int> GenerateFibList(int x);
+
 int main () {
 
+	//std::function<int(int)> Fib = [&Fib](int n){return (n<2)? n : Fib(n-1)+Fib(n-2);};
+	//std::cout<<Fib(3)<<std::endl;
+
+	std::vector<int> listofFibs = GenerateFibList(10);
+	for(auto val: listofFibs)
+		std::cout << val << std::endl;
+
+	return 0;
+}
+
+//std::function<int(int)> Fib = [&Fib](int n){return (n<2)? n : Fib(n-1)+Fib(n-2);};
+
+std::vector<int> GenerateFibList(int x){
+	
 	std::function<int(int)> Fib = 
 	[&Fib](int n){return (n<2)? n : Fib(n-1)+Fib(n-2);};
 
-	std::cout<<Fib(3)<<std::endl;
+	std::vector<int> out;
+	
+	for(int i=0;i<=x;i++){
+		out.push_back(Fib(i));
+	}
 
-	return 0;
+	return out;
 }
