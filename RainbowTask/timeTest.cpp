18cbaa55d8ceeb0b7a3043fb3da28a651fb8c875
Matthew Coyle <matthew@coyle.fr>
2018-09-30 18:18:42 +0200

    RT prototyping00

---
+++ RainbowTask/timeTest.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <fstream>
+#include <ctime>
+
+
+void FileSave ();
+void FileLoad ();
+
+int ParsePassedMoments ();
+int ParsePassedDays ();
+
+int ParseDay ();
+int ParseYear ();
+
+int main () {
+
+}
+
+/*void FileLoad () {
+	ifstream Data("data.txt");
+
+	int day, year;
+	Data >> day >> year
+	while();
+}*/
